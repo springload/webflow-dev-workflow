7b869c656e91da07520f61451644e67de3768ead
HAYDEN PHILLIPS <hayden.phillips@springload.co.nz>
2023-10-02 13:20:04 +1300

    add scss and esbuild-sass-plugin

--- builder.mjs
+++ builder.mjs
@@ -1,14 +1,21 @@
 import * as esbuild from "esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
 
 // entryPoints = "client-projects/[your project]/[the file you're working on]"
 // outfile = "client-projects/[your project]/dist/out.js"
+// outdir = "client-projects/your project/dist"
 
 const context = await esbuild.context({
-  entryPoints: ["client-projects/test-project/index.ts"],
+  entryPoints: [
+    "client-projects/test-project/index.ts",
+    "client-projects/test-project/react-component-test.tsx",
+    "client-projects/test-project/scss/styles.scss",
+  ],
   bundle: true,
   minify: true,
   sourcemap: true,
-  outfile: "client-projects/test-project/dist/out.js",
+  outdir: "client-projects/test-project/dist",
+  plugins: [sassPlugin()],
 });
 
 // Enable watch mode
--- client-projects/test-project/dist/index.js
+++ client-projects/test-project/dist/index.js
@@ -1,2 +1,2 @@
 (()=>{var r=Array.from(document.getElementsByClassName("w--current"));r.forEach(e=>{e.setAttribute("aria-disabled","true"),e.classList.add("pointer-events-none")});console.log("Hello from vs code.");})();
-//# sourceMappingURL=out.js.map
+//# sourceMappingURL=index.js.map
