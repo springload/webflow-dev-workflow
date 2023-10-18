7ab8337bf04da448e84df12785b57cd5945bf880
Sam Muirhead <sam.muirhead@springload.co.nz>
2023-10-19 12:40:35 +1300

    Move working builder file to client project

---
+++ client-projects/the-lumery/builder.mjs
@@ -0,0 +1,24 @@
+import * as esbuild from "esbuild";
+import { sassPlugin } from "esbuild-sass-plugin";
+
+// entryPoints = "client-projects/[your project]/[the file you're working on]"
+// outfile = "client-projects/[your project]/dist/out.js"
+// outdir = "client-projects/your project/dist"
+
+const context = await esbuild.context({
+  entryPoints: [
+    "./client-projects/the-lumery/index.ts",
+    "./client-projects/the-lumery/scss/styles.scss",
+  ],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outdir: "client-projects/the-lumery/dist",
+  plugins: [sassPlugin()],
+});
+
+// Enable watch mode
+await context.watch();
+
+// Enable serve mode
+await context.serve();
