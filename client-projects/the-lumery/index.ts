03186065715eb53757e5791e15f095573735a6b1
Sam Muirhead <sam.muirhead@springload.co.nz>
2023-10-19 10:34:33 +1300

    Add typing effect animation

--- client-projects/the-lumery/index.ts
+++ client-projects/the-lumery/index.ts
@@ -1,7 +1,20 @@
 import heroHeadingAnimation from "./heroHeadingAnimation";
+import { typeTextOn } from "./typeTextOn";
+import { TYPING_LINK_SELECTOR } from "./selectors";
+import {
+  onlyPlayWhenVisible,
+  pauseAnimations,
+  resumeAnimations,
+} from "./utils";
 
 document.addEventListener("DOMContentLoaded", () => {
   // all pages have a hero heading animation.
   // the hero's intersectionObserver setup is contained within the function.
   heroHeadingAnimation();
+
+  const typingLink = document.querySelector(TYPING_LINK_SELECTOR);
+  if (typingLink) {
+    typeTextOn();
+    onlyPlayWhenVisible(TYPING_LINK_SELECTOR);
+  }
 });
