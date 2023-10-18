7798a7007651a11ecd94c4a1e4ad6f67b7ec6c94
Sam Muirhead <sam.muirhead@springload.co.nz>
2023-10-19 11:13:55 +1300

    Add ticker tape animation

--- client-projects/the-lumery/index.ts
+++ client-projects/the-lumery/index.ts
@@ -1,6 +1,6 @@
 import heroHeadingAnimation from "./heroHeadingAnimation";
 import { typeTextOn } from "./typeTextOn";
-import { TYPING_LINK_SELECTOR } from "./selectors";
+import { TYPING_LINK_SELECTOR, TICKER_TAPE_SELECTOR } from "./selectors";
 import {
   onlyPlayWhenVisible,
   pauseAnimations,
@@ -12,9 +12,24 @@ document.addEventListener("DOMContentLoaded", () => {
   // the hero's intersectionObserver setup is contained within the function.
   heroHeadingAnimation();
 
+  // typewriter effect (found on 'yesterday' and 'today' pages)
   const typingLink = document.querySelector(TYPING_LINK_SELECTOR);
   if (typingLink) {
     typeTextOn();
     onlyPlayWhenVisible(TYPING_LINK_SELECTOR);
+  }
+
+  // ticker tape effect (CSS animation, found on 'yesterday' and 'tomorrow' pages)
+  const tickerTapeLink = document.querySelector(TICKER_TAPE_SELECTOR);
+  if (tickerTapeLink) {
+    onlyPlayWhenVisible(TICKER_TAPE_SELECTOR);
+
+    // the ticker tape should pause on hover
+    tickerTapeLink?.addEventListener("mouseover", () =>
+      pauseAnimations(TICKER_TAPE_SELECTOR),
+    );
+    tickerTapeLink?.addEventListener("mouseout", () =>
+      resumeAnimations(TICKER_TAPE_SELECTOR),
+    );
   }
 });
