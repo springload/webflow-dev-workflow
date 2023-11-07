dc07111ad16ee867e22d31da3face90c36506f4c
CliffRobinson <cliff.robinson@springload.co.nz>
2023-11-08 11:42:15 +1300

    BEMify

--- client-projects/the-lumery/js/JobsComponent.tsx
+++ client-projects/the-lumery/js/JobsComponent.tsx
@@ -24,16 +24,7 @@ const getJobsJSON = async (setLoading, setJobs) => {
 
 const JobsDisplay = (isLoading, jobJson) => {
   return (
-    <div className="margin-small">
-      <div className="text-wrapper-hiring">
-        <h2 className="heading-style-h2">Today we're hiring</h2>
-        <div className="padding-bottom padding-small"></div>
-        <div className="text-body-medium">
-          Both data and Technology can be pretty flat without some bright minds
-          and big hearts to bring the magic. That's where you come in.
-        </div>
-      </div>
-      <div className="padding-bottom padding-large"></div>
+    <>
       {isLoading ? (
         <p className="text-wrapper-ad">Loading jobs ...</p>
       ) : (
@@ -42,32 +33,26 @@ const JobsDisplay = (isLoading, jobJson) => {
             <p>We couldn't load the available jobs, please check back later.</p>
           ) : (
             jobJson.map((job) => (
-              <>
-                <a href={job.link} className="ad-link w-inline-block">
-                  <div className="text-wrapper-ad">
-                    <h3 className="heading-style-h3 text-color-black">
-                      {job.name}
-                    </h3>
-                    <div className="padding-bottom padding-small"></div>
-                    <div className="link-ad-wrapper">
-                      <div className="text-body-medium text-color-black">
-                        {job.location}
-                      </div>
+              <div>
+                <a href={job.link} className="jobs__ad">
+                  <div>
+                    <h3 className="jobs__job-header">{job.name}</h3>
+                    <div className="jobs__job-subheader">
+                      <div>{job.location}</div>
                       <img
                         src="https://assets-global.website-files.com/652f0c1212d90b3e8dd3b570/652f0c1212d90b3e8dd3b583_Arrow%205.svg"
                         loading="lazy"
                         alt=""
-                        className="image-8"
+                        className="jobs__job-arrow"
                       />
                     </div>
                   </div>
                 </a>
-                <div className="padding-bottom padding-large"></div>
-              </>
+              </div>
             ))
           )}
         </>
       )}
-    </div>
+    </>
   );
 };
